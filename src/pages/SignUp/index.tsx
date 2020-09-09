40430b5c03fbd71621f3a93c505760abd3bf7188
renatodasilvaborges <renatodasilvaborges@gmail.com>
2020-09-09 00:00:45 -0300

    signin page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,60 @@
-import React from 'react'; 
+import React, { useRef } from 'react'; 
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'; 
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native'; 
+import { Form } from '@unform/mobile'; 
+import { FormHandles } from '@unform/core'; 
 
-import { Container } from './styles'; 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+import logoImg from '../../assets/logo.png'; 
+
+import { Container, 
+         Title, 
+         BackToSignIn, 
+         BackToSignInText 
+    } from './styles'; 
 
 const SignUp: React.FC = () => { 
+    const formRef = useRef<FormHandles>(null); 
+    const navigation = useNavigation(); 
+
     return (
-        <Container /> 
+        <>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }} 
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined }
+            enabled
+        >
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
+            >
+                <Container>
+                    <Image source={logoImg} />
+
+                    <View>
+                        <Title>Faça seu logon</Title>
+                    </View>
+                    <Form ref={formRef} onSubmit={() => {}}>
+                        <Input name="name" icon="user" placeholder="Nome" />
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input name="password" icon="lock" placeholder="Senha" />
+
+                        <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button> 
+                    </Form>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
+        <BackToSignIn onPress={() => {navigation.goBack()}}>
+            <Icon name="arrow-left" size={20} color="#fff"/>
+            <BackToSignInText>Voltar para Logon</BackToSignInText>
+        </BackToSignIn>
+
+        </>
     );
 }; 
 
